337bc137be438a632ec3ca28d7ec034305626187
David Svezhitnsev <daveranan@gmail.com>
2020-05-11 14:11:13 -0500

    Init

---
+++ src/components/layout/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav className='nav'>
+    <a href='/' className='nav__link'>
+      &larr; Назад
+    </a>
+  </nav>
+);
+
+export default Navigation;
